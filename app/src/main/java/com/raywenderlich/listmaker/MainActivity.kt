7c7e5e3dd054c893939cff1fd185339437c33ac8
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-02-04 14:56:51 -0500

    The layout file for the tablet device has been added.

--- app/src/main/java/com/raywenderlich/listmaker/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/listmaker/MainActivity.kt
@@ -19,85 +19,88 @@ import com.raywenderlich.listmaker.ui.main.MainViewModelFactory
 
 class MainActivity : AppCompatActivity(), MainFragment.MainFragmentInteractionListener {
 
-  private lateinit var binding: MainActivityBinding
+    private lateinit var binding: MainActivityBinding
 
-  private lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: MainViewModel
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    viewModel = ViewModelProvider(
-        this,
-        MainViewModelFactory(PreferenceManager.getDefaultSharedPreferences(this))
-    )
-      .get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(
+            this,
+            MainViewModelFactory(PreferenceManager.getDefaultSharedPreferences(this))
+        )
+            .get(MainViewModel::class.java)
 
-    binding = MainActivityBinding.inflate(layoutInflater)
-    val view = binding.root
-    setContentView(view)
-    Log.i("MainActivity", viewModel.toString())
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        Log.i("MainActivity", viewModel.toString())
 
-    if (savedInstanceState == null) {
-      val mainFragment = MainFragment.newInstance(this)
-      supportFragmentManager.beginTransaction()
-        .replace(R.id.detail_container, mainFragment)
-        .commitNow()
+        if (savedInstanceState == null) {
+            val mainFragment = MainFragment.newInstance(this)
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.detail_container, mainFragment)
+                .commitNow()
+        }
+
+        binding.fabButton.setOnClickListener {
+            showCreateListDialog()
+        }
     }
 
-    binding.fabButton.setOnClickListener {
-      showCreateListDialog()
-    }
-  }
+    private fun showCreateListDialog() {
 
-  private fun showCreateListDialog() {
+        val dialogTitle = getString(R.string.name_of_list)
+        val positiveButtonTitle = getString(R.string.create_list)
 
-    val dialogTitle = getString(R.string.name_of_list)
-    val positiveButtonTitle = getString(R.string.create_list)
+        val builder = AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+        listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
 
-    val builder = AlertDialog.Builder(this)
-    val listTitleEditText = EditText(this)
-    listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setTitle(dialogTitle)
+        builder.setView(listTitleEditText)
+        builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
+            dialog.dismiss()
 
-    builder.setTitle(dialogTitle)
-    builder.setView(listTitleEditText)
-    builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
-      dialog.dismiss()
+            val taskList = TaskList(listTitleEditText.text.toString())
+            viewModel.saveList(taskList)
+            showListDetail(taskList)
+        }
 
-      val taskList = TaskList(listTitleEditText.text.toString())
-      viewModel.saveList(taskList)
-      showListDetail(taskList)
+        builder.create().show()
     }
 
-    builder.create().show()
-  }
-
-  private fun showListDetail(list: TaskList) {
-    val listDetailIntent = Intent(this, ListDetailActivity::class.java)
-    listDetailIntent.putExtra(INTENT_LIST_KEY, list)
-
-    startActivityForResult(listDetailIntent, LIST_DETAIL_REQUEST_CODE)
-  }
-
-  override fun listItemTapped(list: TaskList) {
-    showListDetail(list)
-  }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data:
-  Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    // 1
-    if (requestCode == LIST_DETAIL_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-      // 2
-      data?.let {
-        // 3
-        viewModel.updateList(data.getParcelableExtra(INTENT_LIST_KEY)!!)
-        viewModel.refreshLists()
-      }
+    private fun showListDetail(list: TaskList) {
+        val listDetailIntent = Intent(this, ListDetailActivity::class.java)
+        //in the conversation between the activities, INTENT_LIST_KEY and LIST_DETAIL_REQUEST_CODE show that the request is coming from each activity and the response coming from which one.
+        listDetailIntent.putExtra(INTENT_LIST_KEY, list)
+        //waiting until hearing back from the list detail activity.
+        startActivityForResult(listDetailIntent, LIST_DETAIL_REQUEST_CODE)
     }
-  }
 
-  companion object {
-    const val INTENT_LIST_KEY = "list"
-    const val LIST_DETAIL_REQUEST_CODE = 123
-  }
+    override fun listItemTapped(list: TaskList) {
+        showListDetail(list)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, data:
+        Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // 1
+        if (requestCode == LIST_DETAIL_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // 2
+            data?.let {
+                // 3
+                viewModel.updateList(data.getParcelableExtra(INTENT_LIST_KEY)!!)
+                viewModel.refreshLists()
+            }
+        }
+    }
+
+    companion object {
+        const val INTENT_LIST_KEY = "list"
+        const val LIST_DETAIL_REQUEST_CODE = 123
+    }
 }
